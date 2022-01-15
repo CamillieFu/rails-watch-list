b7a786b54d781350bf90dd3210d1b5f7dcdf80ca
Camille Fujisaki <cae224@cornell.edu>
2022-01-15 11:20:39 +0900

    models completed sans uniqe scope in bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, presence: true
+  validates :comment, length: { minimum: 6 }
+  validates :comment, uniqueness: { scope: [:list_id, :movie_id] }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
-  has_many :movies, through: :bookmarks
+  has_many :bookmarks
+  has_many :movies, through: :bookmarks, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
 end
